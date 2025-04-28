ad8a5ca0ee119566417c9bc6d5d2bf159b45ae09
Jessica Wang <jessicaw.040131@gmail.com>
2025-04-27 19:31:14 -0700

    storyboard version

---
+++ ikidTests/ikid_tempTests.swift
@@ -0,0 +1,17 @@
+//
+//  ikid_tempTests.swift
+//  ikid tempTests
+//
+//  Created by Jessica  Wang on 4/27/25.
+//
+
+import Testing
+@testable import ikid_temp
+
+struct ikid_tempTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
